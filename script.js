6bc236b71f5a82293afee57f3297896f4c2e0201
psherrard <patsherrard@gmail.com>
2020-07-07 13:07:07 -0400

    cal logic pretty sound, not to redo layout

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
 const date = new Date();
 
-// date.setDate(1);
+const renderCal = () => {
 
-const monthDays = document.querySelector('.days');
+    date.setDate(1);
 
-const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const monthDays = document.querySelector('.days');
 
-console.log(lastDay);
+    // +1 gives last day of current month
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    // give last days of prev month
+    const prevLastDay =  new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-let months = [
-    'January',
-    'Feburary',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-];
+    const firstDayIndex = date.getDay();
 
-document.querySelector('.date h1').innerHTML
-= months[date.getMonth()];
+    //getDay return index number
+    const lastDayIndex =  new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+    console.log(lastDayIndex);
 
-document.querySelector('.date p').innerHTML
-= date.toDateString();
+    const nextDays = 7 - lastDayIndex - 1;
+    console.log(nextDays);
 
-let days = '';
+    let months = [
+        'January',
+        'Feburary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
 
-for(let i = 1 ; i<= lastDay; i++) {
-    days += `<div>${i}</div>`
-    monthDays.innerHTML = days;
+    document.querySelector('.date h1').innerHTML
+    = months[date.getMonth()];
+
+    document.querySelector('.date p').innerHTML
+    = new Date().toDateString();
+
+    let days = '';
+
+    for(let x = firstDayIndex; x > 0; x-- ){
+        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    }
+
+    for(let i = 1 ; i<= lastDay; i++) {
+        if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+            days += `<div class="today">${i}</div>`
+        } else {
+            days += `<div>${i}</div>`  
+        }
+    }
+
+    for(let j = 1; j <= nextDays; j++){
+        days += `<div class="next-date">${j}</div>`
+        monthDays.innerHTML = days;
+    };
+
 }
 
 
+
+
+
+
+document.querySelector('.prev').addEventListener('click', () => {
+    date.setMonth(date.getMonth() -  1)
+    renderCal();
+})
+
+document.querySelector('.next').addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1)
+    renderCal();
+})
+
+renderCal();
